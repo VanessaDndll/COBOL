2b0c191f8c66ca976da8af53fe3295af55d6eaba
Vanessa <vanessadonadelli2014@hotmail.com>
2024-11-19 22:13:03 -0300

    comando repetição - varying

--- Primeiro_programa.cbl
+++ Primeiro_programa.cbl
@@ -758,8 +758,8 @@
        P100-INICIO.
            INITIALISE WS-VARIAVEIS.
       *     PERFORM P300-PROCESSA-1     THRU P300-FIM 3 TIMES
-           PERFORM P500-PROCESSA-2     THRU P500-FIM WITH TEST
-                                       BEFORE UNTIL WS-COUNT = 5
+      *     PERFORM P500-PROCESSA-2     THRU P500-FIM WITH TEST
+      *                                 BEFORE UNTIL WS-COUNT = 5
            PERFORM P700-PROCESSA-3     THRU P700-FIM
            PERFORM P900-FINALIZA
            .
@@ -778,20 +778,24 @@
       * P300-FIM.
       *
       ******* UNTIL
-       P500-PROCESSA-2.
-           ADD 1                       TO WS-COUNT
-           DISPLAY "CONTADOR: " WS-COUNT
-
+      * P500-PROCESSA-2.
+      *     ADD 1                       TO WS-COUNT
+      *     DISPLAY "CONTADOR: " WS-COUNT
+      *
       *     MOVE ZEROS                  TO WS-TOT
-           PERFORM WITH TEST BEFORE UNTIL WS-TOT = 3
-           ADD 1                       TO WS-TOT
-           DISPLAY 'WS-TOT: ' WS-TOT
+      *     PERFORM WITH TEST BEFORE UNTIL WS-TOT = 3
+      *     ADD 1                       TO WS-TOT
+      *     DISPLAY 'WS-TOT: ' WS-TOT
+      *     END-PERFORM
+      *     .
+      * P500-FIM.
+      *
+      ******* VARYING
+       P700-PROCESSA-3.
+           PERFORM VARYING WS-IND FROM 2 BY 2 UNTIL WS-IND GREATER 5
+           DISPLAY 'WS-IND: ' WS-IND
            END-PERFORM
            .
-       P500-FIM.
-
-       P700-PROCESSA-3.
-
        P700-FIM.
 
        P900-FINALIZA.
